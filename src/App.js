a27a3c185f1718c4a28d24bc8796a1e094217db6
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-07-04 12:27:06 +0200

    add loadingspinner and doc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Button, LoadingSpinner } from "./lib";
+
 function App() {
-  return <div className="App">Axona UI library</div>;
+  return (
+    <>
+      <div className="App">Axona UI library</div>
+      <Button
+        onClick={() => {
+          console.log("click");
+        }}
+      >
+        clicca
+      </Button>
+      <LoadingSpinner></LoadingSpinner>
+    </>
+  );
 }
 
 export default App;
--- src/lib/components/LoadingSpinner/LoadingSpinner.js
+++ src/lib/components/LoadingSpinner/LoadingSpinner.js
@@ -1,4 +1,5 @@
 import classes from "../style/LoadingSpinner.module.css";
+import React from "react";
 
 const LoadingSpinner = () => {
   return <div className={classes.spinner}></div>;
