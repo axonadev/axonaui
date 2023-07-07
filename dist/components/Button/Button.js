375582389b3d7d52b295e81957e0c93c2e6131a1
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-07-07 09:15:29 +0200

    add path in sidemenu

---
+++ dist/components/Button/Button.js
@@ -0,0 +1,30 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+var _ButtonModule = _interopRequireDefault(require("../style/Button.module.css"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const Button = _ref => {
+  let {
+    children,
+    id,
+    onClick,
+    className
+  } = _ref;
+  const clsStyle = [_ButtonModule.default.button, className];
+  const onClickHandler = evt => {
+    try {
+      onClick(evt);
+    } catch (error) {}
+  };
+  return /*#__PURE__*/_react.default.createElement("button", {
+    id: id,
+    className: clsStyle.join(" "),
+    onClick: onClickHandler
+  }, children);
+};
+var _default = Button;
+exports.default = _default;
