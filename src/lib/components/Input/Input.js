91a6eb0c4b6774dd2f58d9e5fe165ffcf655fc39
marcocareddu <saldumaccu@tuta.io>
2024-03-07 15:36:28 +0100

    aggiunto disabled a textarea

--- src/lib/components/Input/Input.js
+++ src/lib/components/Input/Input.js
@@ -169,6 +169,7 @@ const Input = ({
             onBlur={onBlurInput}
             onFocus={InputFocus}
             value={InputValue}
+            disabled={disabled}
           ></textarea>
         )}
 
