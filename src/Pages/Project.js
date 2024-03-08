804a9663ffd34436a9441df4ef201b7553c93fb2
marcocareddu <saldumaccu@tuta.io>
2024-03-08 09:33:00 +0100

    fix(rimossa clase success per testata)

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -126,7 +126,7 @@ const Project = ({ request, list, help }) => {
       <Frame
         label='TESTATA'
         type='form_t'
-        stato={"success"}
+        stato={""}
         ridimensiona={true}
         setup={true}
         icon={"faAddressCard"}
