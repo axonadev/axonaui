4674a293ce5292dc8186fb50d5362a9057d6eaf5
marcocareddu <saldumaccu@tuta.io>
2024-03-11 11:36:07 +0100

    fix(completamente rimosso lo store)

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { sideMenuOpenReducer } from "./SideMenuOpenSlice";
-
-export default configureStore({
-  reducer: {
-    // Inserire qui i reducer
-    sideMenu: sideMenuOpenReducer,
-  },
-});
