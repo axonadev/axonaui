33a5ab7ced201194c1687dd10664a9aff16402fd
marcocareddu <saldumaccu@tuta.io>
2024-03-19 15:30:37 +0100

    fix(TextEditor modificato)

--- src/lib/components/TextEditor/TextEditor.js
+++ src/lib/components/TextEditor/TextEditor.js
@@ -53,6 +53,20 @@ const TextEditor = forwardRef(
                 "list",
                 "image",
               ],
+              inline: {
+                inDropdown: false,
+                className: undefined,
+                component: undefined,
+                dropdownClassName: undefined,
+                options: ["bold", "italic", "underline", "strikethrough"],
+              },
+              list: {
+                inDropdown: false,
+                className: undefined,
+                component: undefined,
+                dropdownClassName: undefined,
+                options: ["unordered", "ordered"],
+              },
             }}
             localization={{
               locale: "it",
